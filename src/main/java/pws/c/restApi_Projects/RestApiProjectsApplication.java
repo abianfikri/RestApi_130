f1722adf192e7d33d5f039ec88ebbda76a3411c9
abianfikri <abronjoy46@gmail.com>
2022-12-04 13:34:46 +0700

    first Project Rest API

---
+++ src/main/java/pws/c/restApi_Projects/RestApiProjectsApplication.java
@@ -0,0 +1,13 @@
+package pws.c.restApi_Projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/c/restApi_Projects/RestApiProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.restApi_Projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
