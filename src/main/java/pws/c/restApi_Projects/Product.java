d3eabb8eb2f42a88e7352331684aa5522e746298
abianfikri <abronjoy46@gmail.com>
2022-12-04 15:37:53 +0700

    edit file Product, menambahkan dan manipulasi POST dan PUT di file Controller

--- src/main/java/pws/c/restApi_Projects/Product.java
+++ src/main/java/pws/c/restApi_Projects/Product.java
@@ -13,11 +13,6 @@ public class Product {
     private String id;
     private String name;
 
-    public Product(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
     public String getId() {
         return id;
     }
---
+++ src/main/java/pws/c/restApi_Projects/ProductServiceController.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pws.c.restApi_Projects;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Abian
+ */
+@RestController
+public class ProductServiceController {
+    private static HashMap<String, Product > productRepo = new HashMap<>();
+    
+    static{
+        Product honey = new Product();
+        honey.setId("1");
+        honey.setName("Madu");
+        productRepo.put(honey.getId(), honey);
+        
+        Product almond = new Product();
+        almond.setId("2");
+        almond.setName("almond");
+        productRepo.put(almond.getId(), almond);
+    }
+    
+    // Get API
+    @RequestMapping(value="/products")
+    public ResponseEntity<Object> getProduct(){
+       return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+    
+    // Post API
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product){
+        
+        if(productRepo.containsKey(product.getId())){
+            return new ResponseEntity<>("Data tidak Boleh sama", HttpStatus.OK);
+        }
+        else{
+            productRepo.put(product.getId(), product);
+            return new ResponseEntity<>("Product is created Successfully", HttpStatus.CREATED);
+        }
+    }
+    
+    // PUT API
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product){
+        
+        if(!productRepo.containsKey(id)){
+            return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+        }
+        else{
+            productRepo.remove(id);
+            product.setId(id);
+            productRepo.put(id, product);
+            return new  ResponseEntity<>("Product is updated Successfully",HttpStatus.OK);
+        }
+        
+    }
+    
+    // DELETE API
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") String id){
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);
+    }
+    
+}
